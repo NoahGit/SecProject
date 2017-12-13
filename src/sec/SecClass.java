3393459930fe1f525c28f9bb316c862c4b921364
noah <gaoyong@xatu.edu.cn>
2017-12-13 15:25:39 +0800

    20171213提交的

--- src/sec/SecClass.java
+++ src/sec/SecClass.java
@@ -4,7 +4,7 @@ public class SecClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("µÚÈý´Î");
+		System.out.println("20171213");
 	}
 
 }
