f0dea9b79d7bfcd6289944d8ad3836230395f21a
noah <gaoyong@xatu.edu.cn>
2017-12-20 10:54:55 +0800

    jiale wu hang sysout

--- src/sec/SecClass.java
+++ src/sec/SecClass.java
@@ -5,6 +5,11 @@ public class SecClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("20171213");
+		System.out.println("20171213");
+		System.out.println("20171213");
+		System.out.println("20171213");
+		System.out.println("20171213");
+		
 	}
 
 }
